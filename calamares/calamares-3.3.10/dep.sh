2e266262e23202505558abe624237a46fe7aa5b8
Akash6222 <akashdubey25274@gmail.com>
2024-10-28 00:24:44 +0530

    ADD: calamares 3.10

---
+++ calamares/calamares-3.3.10/dep.sh
@@ -0,0 +1,6 @@
+# Installing calamares dependencies for ALG
+
+echo "Installing: kconfig kcoreaddons kiconthemes ki18n kio solid yaml-cpp kpmcore mkinitcpio-openswap ckbcomp hwinfo qt6-svg polkit-qt6 gtk-update-icon-cache squashfs-tools libpwquality pybind11 python extra-cmake-modules qt6-tools qt6-translations git"
+
+sudo pacman -Sy kconfig kcoreaddons kiconthemes ki18n kio solid yaml-cpp kpmcore mkinitcpio-openswap ckbcomp hwinfo qt6-svg polkit-qt6 gtk-update-icon-cache squashfs-tools libpwquality pybind11 python extra-cmake-modules qt6-tools qt6-translations git
+
